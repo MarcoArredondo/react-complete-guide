215c7e5f0cb7f34275e409e353428e90882fef1d
Marco Arredondo <marcoarredondoz28@gmail.com>
2022-12-23 13:24:48 -0600

    Adding second component to render only the date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,13 @@
 import React from "react";
+import { ExpenseDate } from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props){
 
     const {title, amount, date} = props;
-
-    const month = date.toLocaleString('en-US', {month: 'long'});
-    const day = date.toLocaleString('en-US', {day: '2-digit'});
-    const year = date.getFullYear();
-
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{day}</div>
-                <div>{year}</div>
-            </div>
+            <ExpenseDate date={date}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>{title}</h2>
                 <div className="expense-item__price">${amount}</div>

80cc726ebc83d752805891ccc89c990d35df5e0f
Marco Arredondo <marcoarredondoz28@gmail.com>
2022-12-23 13:11:40 -0600

    Passing props from parent component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,24 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-function ExpenseItem(){
+function ExpenseItem(props){
 
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = "Car Insurance";
-    const expenseAmount = 291.67;
+    const {title, amount, date} = props;
+
+    const month = date.toLocaleString('en-US', {month: 'long'});
+    const day = date.toLocaleString('en-US', {day: '2-digit'});
+    const year = date.getFullYear();
 
     return (
         <div className="expense-item">
             <div>
-                {expenseDate.toISOString()}
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
             </div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">${expenseAmount}</div>
+                <h2>{title}</h2>
+                <div className="expense-item__price">${amount}</div>
             </div>
         </div>
     );
